5a22156c9fa3604be7b62f76ceaef7ae580706b7
ifaint <dzhuang@scut.edu.cn>
2021-08-21 05:40:48 +0800

    added 'sphinx.ext.todo' extension for sphinx

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -39,7 +39,8 @@ release = '1.1.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc',
+extensions = ['sphinx.ext.todo',
+              'sphinx.ext.autodoc',
               'sphinx.ext.autosectionlabel',
               'djangodocs',
               'sphinx.ext.viewcode'
