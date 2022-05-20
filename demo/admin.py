8f785aad87de99b9ed2a6f110281b1b3e2cdb938
dzhuang <dzhuang.scut@gmail.com>
2022-05-20 11:46:02 +0800

    Added GalleryAdminFormMixin in admin of demo app to prepend bootstrap dependencies.

--- demo/admin.py
+++ demo/admin.py
@@ -1,7 +1,31 @@
+from django import forms
 from django.contrib import admin
 
 from demo.models import DemoGallery
 from galleryfield.models import BuiltInGalleryImage
 
-admin.site.register(DemoGallery)
+
+class GalleryAdminFormMixin:
+    class Media:
+        css = {
+            "all": ["https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.4.1/css/bootstrap.css"]  # noqa
+        }
+
+        js = (
+            "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js",
+            "https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.4.1/js/bootstrap.min.js"  # noqa
+        )
+
+
+class DemoGalleryAdminForm(GalleryAdminFormMixin, forms.ModelForm):
+    class Meta:
+        model = DemoGallery
+        exclude = ()
+
+
+class DemoGalleryAdmin(admin.ModelAdmin):
+    form = DemoGalleryAdminForm
+
+
+admin.site.register(DemoGallery, DemoGalleryAdmin)
 admin.site.register(BuiltInGalleryImage)
--- demo_custom/admin.py
+++ demo_custom/admin.py
@@ -1,6 +1,19 @@
+from django import forms
 from django.contrib import admin
 
+from demo.admin import GalleryAdminFormMixin
 from demo_custom.models import CustomDemoGallery, CustomImage
 
-admin.site.register(CustomDemoGallery)
+
+class CustomDemoGalleryAdminForm(GalleryAdminFormMixin, forms.ModelForm):
+    class Meta:
+        model = CustomDemoGallery
+        exclude = ()
+
+
+class CustomDemoGalleryAdmin(admin.ModelAdmin):
+    form = CustomDemoGalleryAdminForm
+
+
+admin.site.register(CustomDemoGallery, CustomDemoGalleryAdmin)
 admin.site.register(CustomImage)
