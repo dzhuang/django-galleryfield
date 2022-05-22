bd8b0440721d3948eeeb3b016c93104a586ccd6a
dzhuang <dzhuang.scut@gmail.com>
2022-05-22 19:43:32 +0800

    Update bundle-source.js and jquery.fileupload-ui-gallery-widget.js

--- galleryfield/static/js/bundle-source.js
+++ galleryfield/static/js/bundle-source.js
@@ -3,34 +3,14 @@ import './jquery-importer';
 /* This is necessary for sortable */
 import 'jquery-ui-dist/jquery-ui.min';
 
-import 'blueimp-tmpl/js/tmpl.min';
-
-import 'blueimp-load-image/js/load-image.all.min';
-
-import 'blueimp-canvas-to-blob/js/canvas-to-blob.min';
-
 import 'blueimp-gallery/js/jquery.blueimp-gallery.min';
 
 import 'bootstrap';
-
-import 'blueimp-file-upload/js/jquery.iframe-transport';
-
-import 'blueimp-file-upload/js/jquery.fileupload';
-
-import 'blueimp-file-upload/js/jquery.fileupload-process';
-
-import 'blueimp-file-upload/js/jquery.fileupload-image';
-
-import 'blueimp-file-upload/js/jquery.fileupload-audio';
-
-import 'blueimp-file-upload/js/jquery.fileupload-video';
-
-import 'blueimp-file-upload/js/jquery.fileupload-validate';
 
 import 'blueimp-file-upload/js/jquery.fileupload-ui';
 
 import 'cropper/dist/cropper.min';
 
-import './jquery.fileupload-ui-gallery-widget';
+// import './jquery.fileupload-ui-gallery-widget';
 
 import '../css/bundle.css';
