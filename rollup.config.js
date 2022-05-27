2296f61df5d6a5fb9e58c73f59b49af6b851f9d4
dzhuang <dzhuang.scut@gmail.com>
2022-05-28 03:57:17 +0800

    Switch to material design icons from fontawesome.

--- rollup.config.js
+++ rollup.config.js
@@ -5,26 +5,7 @@ import commonjs from '@rollup/plugin-commonjs';
 import { terser } from 'rollup-plugin-terser';
 import styles from 'rollup-plugin-styles';
 import gzipPlugin from 'rollup-plugin-gzip';
-// import json from '@rollup/plugin-json';
-
-import materialDesignIconSubset from 'material-icons-subset2';
-
-materialDesignIconSubset();
-
-
-// {{{  extra fontawesome subset, need to edit
-// galleryfield/static/dev/@fortawesome/fontawesome-free/css/all.css accordingly after first run.
-import { fontawesomeSubset } from "fontawesome-subset";
-fontawesomeSubset(
-    [
-        // yes, rotate-left, rotate-right, close, reset, warning
-        'check', 'reply', 'share', 'times', 'sync-alt', 'exclamation-triangle',
-        // add, edit, start, cancel, delete
-        'plus', 'edit', 'upload', 'ban', 'trash-alt'
-    ],
-    'galleryfield/static/dev/@fortawesome/fontawesome-free/webfonts'
-);
-// }}}
+import {mdiSubset} from 'materialdesignicon-subset';
 
 // `npm run build` -> `production` is true
 // `npm run dev` -> `production` is false
@@ -32,17 +13,31 @@ const production = !process.env.ROLLUP_WATCH;
 
 const brotliPromise = promisify(brotliCompress);
 
+const mdiFonts = [
+    {name: "plus", transformOptions: {viewBox: [4, 4, 16, 16]}},
+    {name: "check", transformOptions: {viewBox: [3, 3, 18, 18]}},
+    {name: "trash-can", transformOptions: {viewBox: [3, 3, 18, 18]}},
+    {name: "format-rotate-90", transformOptions: {viewBox: [1, 1, 22, 22]}},
+    {name: "upload", transformOptions: {viewBox: [3, 3, 18, 18]}},
+    {name: "cancel", transformOptions: {viewBox: [2, 2, 20, 20]}},
+    {name: "reload", transformOptions: {viewBox: [2, 2, 20, 20]}},
+    {name: "close", transformOptions: {viewBox: [3, 3, 18, 18]}},
+    {name: "sync", transformOptions: {viewBox: [1, 1, 22, 22]}},
+    "crop-rotate",
+];
+
+mdiSubset(mdiFonts, "galleryfield/static/dev/mdiFonts");
+
 const defaultPlugins = [
   resolve(),
   styles(),
   commonjs(),
-  // json(),
   production && terser(), // minify, but only in production
   production && gzipPlugin(),
   production && gzipPlugin({
     customCompression: (content) => brotliPromise(Buffer.from(content)),
     fileName: '.br',
-  })
+  }),
 ];
 
 export default [
@@ -61,8 +56,6 @@ export default [
     external: [
         "jquery",
         "bootstrap",
-        // "fabric",
-        // 'tui-code-snippet'
     ]
   },
 ];
