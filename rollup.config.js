27b1be633146d3ca16772883a085033dde4a0192
dzhuang <dzhuang.scut@gmail.com>
2022-06-03 21:19:40 +0800

    Use new build script via subset-iconfont.

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,11 @@
 import resolve from '@rollup/plugin-node-resolve';
-import { brotliCompress } from 'zlib';
-import { promisify } from 'util';
+import {brotliCompress} from 'zlib';
+import {promisify} from 'util';
 import commonjs from '@rollup/plugin-commonjs';
-import { terser } from 'rollup-plugin-terser';
+import {terser} from 'rollup-plugin-terser';
 import styles from 'rollup-plugin-styles';
 import gzipPlugin from 'rollup-plugin-gzip';
-import {mdiSubset} from 'materialdesignicon-subset';
+import {subsetIconfont, MdiProvider, FaFreeProvider} from 'subset-iconfont';
 
 // `npm run build` -> `production` is true
 // `npm run dev` -> `production` is false
@@ -13,20 +13,26 @@ const production = !process.env.ROLLUP_WATCH;
 
 const brotliPromise = promisify(brotliCompress);
 
-const mdiFonts = [
-    {name: "plus", transformOptions: {viewBox: [4, 4, 16, 16]}},
-    {name: "check", transformOptions: {viewBox: [3, 3, 18, 18]}},
-    {name: "trash-can", transformOptions: {viewBox: [3, 3, 18, 18]}},
-    {name: "format-rotate-90", transformOptions: {viewBox: [1, 1, 22, 22]}},
-    {name: "upload", transformOptions: {viewBox: [3, 3, 18, 18]}},
-    {name: "cancel", transformOptions: {viewBox: [2, 2, 20, 20]}},
-    {name: "reload", transformOptions: {viewBox: [2, 2, 20, 20]}},
-    {name: "close", transformOptions: {viewBox: [3, 3, 18, 18]}},
-    {name: "sync", transformOptions: {viewBox: [1, 1, 22, 22]}},
+const mdi = new MdiProvider([
+    "format-rotate-90",
+    "cancel",
+    "reload",
+    "sync",
     "crop-rotate",
-];
+  ],
+);
 
-mdiSubset(mdiFonts, "galleryfield/static/dev/mdiFonts");
+const fa = new FaFreeProvider([
+    "plus",
+    "check",
+    "trash-can",
+    "upload",
+    "triangle-exclamation",
+    "xmark",
+  ],
+);
+
+subsetIconfont([mdi, fa], "galleryfield/static/dev/ss-iconfont");
 
 const defaultPlugins = [
   resolve(),
@@ -54,8 +60,8 @@ export default [
     },
     plugins: defaultPlugins,
     external: [
-        "jquery",
-        "bootstrap",
+      "jquery",
+      "bootstrap",
     ]
   },
 ];
