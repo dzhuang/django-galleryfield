50dfd27041be52e30bf7ee626d4704a0e87f7fcb
dzhuang <dzhuang.scut@gmail.com>
2022-07-17 18:36:56 +0800

    Update style of jquery.fileupload-ui-gallery-widget.js

--- galleryfield/static/js/bundle-source.js
+++ galleryfield/static/js/bundle-source.js
@@ -1,11 +1,6 @@
 import './module-importer';
-
 import 'blueimp-gallery/js/jquery.blueimp-gallery.min';
-
 import 'bootstrap';
-
 import 'blueimp-file-upload/js/jquery.fileupload-ui';
-
 import './jquery.fileupload-ui-gallery-widget';
-
 import '../css/bundle.css';
