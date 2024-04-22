7b91ba5cc5e8225e6d6a8913d116fe2c2283a25b
dzhuang <dzhuang.scut@gmail.com>
2024-04-22 18:24:57 +0800

    Added admin.py and fixed urls.

--- demo_multiple_fields_1_model/admin.py
+++ demo_multiple_fields_1_model/admin.py
@@ -1,1 +1,19 @@
+from django import forms
+from django.contrib import admin
+
+from demo_multiple_fields_1_model.models import MyGallery
+from galleryfield.mixins import GalleryFormMediaMixin
+
+
+class MyGalleryAdminForm(GalleryFormMediaMixin, forms.ModelForm):
+    class Meta:
+        model = MyGallery
+        exclude = ()
+
+
+class MyGalleryAdmin(admin.ModelAdmin):
+    form = MyGalleryAdminForm
+
+
+admin.site.register(MyGallery, MyGalleryAdmin)
 
--- demo_multiple_fields_1_model/urls.py
+++ demo_multiple_fields_1_model/urls.py
@@ -5,31 +5,31 @@ urlpatterns = [
     path(
         "upload1/",
         image_views.MyImage1CreateView.as_view(),
-        name="event-myimage1-upload",
+        name="demo_multiple_fields_1_model-myimage1-upload",
     ),
     path(
         "fetch1/",
         (image_views.MyImage1ListView.as_view()),
-        name="event-myimage1-fetch",
+        name="demo_multiple_fields_1_model-myimage1-fetch",
     ),
     path(
         "crop1/<int:pk>",
         image_views.MyImage1CropView.as_view(),
-        name="event-myimage1-crop",
+        name="demo_multiple_fields_1_model-myimage1-crop",
     ),
     path(
         "upload2/",
         image_views.MyImage2CreateView.as_view(),
-        name="event-myimage2-upload",
+        name="demo_multiple_fields_1_model-myimage2-upload",
     ),
     path(
         "fetch2/",
         (image_views.MyImage2ListView.as_view()),
-        name="event-myimage2-fetch",
+        name="demo_multiple_fields_1_model-myimage2-fetch",
     ),
     path(
         "crop2/<int:pk>",
         image_views.MyImage2CropView.as_view(),
-        name="event-myimage2-crop",
+        name="demo_multiple_fields_1_model-myimage2-crop",
     ),
 ]
